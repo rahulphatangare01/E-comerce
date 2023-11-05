e404d311e44f03c0523b50fa9fb856b37df70aa8
Rahul phatangare <rahulphatangare01@gmail.com>
2023-11-06 00:03:27 +0530

    update & delete product api created

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,5 +1,6 @@
-const Product = require("../models/productModel");
 
+const Product = require("../models/productModel");
+const mongoose = require('mongoose');
 // Create Product --Admin
 exports.CreateProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -13,6 +14,94 @@ exports.CreateProduct = async (req, res, next) => {
 //  get All Products
 exports.getAllProducts = async (req, res) => {
   const products = await Product.find();
-  
+
   res.status(200).json({ success: true, products });
+};
+
+
+//  update product --Admin
+
+exports.updateProduct = async (req,res,next)=> {
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success:false,
+            message:"Product not found"
+        })
+    }
+
+    const products = await Product.findByIdAndUpdate(req.params.id,req.body,{
+        new:true,
+        runValidators:true,
+        useFindAndModify:false
+     })
+     res.status(200).json({
+        success:true,
+        products
+     })
+}
+
+//  Delete product --Admin 
+
+// exports.deleteProduct = async (req,res)=>{
+//   const product = await Product.findById(req.params.id);
+
+//   if(!product){
+//       return res.status(500).json({
+//           success:false,
+//           message:"Product not found"
+//       })
+//   }
+
+//   const  products = await Product.findByIdAndRemove(req.params.id);
+
+//   res.status({
+//     success:true,
+//     message:"Product deleted successfully"
+//   })
+
+  
+
+// }
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid product ID"
+      });
+    }
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        success: false,
+        message: "Product not found"
+      });
+    }
+
+    const removedProduct = await Product.findByIdAndDelete(req.params.id);
+   
+
+    if (removedProduct) {
+      return res.status(200).json({
+        success: true,
+        message: "Product deleted successfully"
+      });
+    } else {
+      return res.status(500).json({
+        success: false,
+        message: "Error deleting product"
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error"
+    });
+  }
 };
--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { getAllProducts, CreateProduct } = require('../controller/productController');
+const { getAllProducts, CreateProduct, updateProduct, deleteProduct  } = require('../controller/productController');
 
 const router = express.Router();
 
-router.route('/product').get(getAllProducts)
-router.route('/product/new').post(CreateProduct)
+router.route('/product').get(getAllProducts);
+router.route('/product/new').post(CreateProduct);
+router.route('/product/:id').put(updateProduct);
+router.route('/product/:id').delete(deleteProduct);
+
 
 
 
