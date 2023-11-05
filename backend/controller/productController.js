a9b8f1f3e445e9b9f55d6c8f3bb052b876637180
Rahul phatangare <rahulphatangare01@gmail.com>
2023-11-05 10:59:29 +0530

    product modal created

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,4 +1,18 @@
-exports.getAllProducts = (req,res)=>{
-    res.status(200).json({message:"route is working "})
+const Product = require("../models/productModel");
 
-}
+// Create Product --Admin
+exports.CreateProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+//  get All Products
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  
+  res.status(200).json({ success: true, products });
+};
--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getAllProducts } = require('../controller/productController');
+const { getAllProducts, CreateProduct } = require('../controller/productController');
 
 const router = express.Router();
 
 router.route('/product').get(getAllProducts)
+router.route('/product/new').post(CreateProduct)
+
 
 
 module.exports = router
