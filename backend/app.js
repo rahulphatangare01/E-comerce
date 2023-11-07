ebfec0bb4ced7b601697c1723ed13f78776e1268
Rahul phatangare <rahulphatangare01@gmail.com>
2023-11-08 00:18:06 +0530

    created auth route

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-
+const cookieParser = require('cookie-parser');
 const errorMiddleware = require('./middlewares/error');
 
 app.use(express.json())
-
+app.use(cookieParser());
 // Routes 
 const product =require('./routes/productsRoute');
 const user = require('./routes/userRoute')
--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { getAllProducts, CreateProduct, updateProduct, deleteProduct, getProductDetails  } = require('../controller/productController');
+const { isAuthanticatedUser } = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.route('/product').get(getAllProducts);
+router.route('/product').get(isAuthanticatedUser,getAllProducts);
 router.route('/product/new').post(CreateProduct);
 router.route('/product/:id').put(updateProduct);
 router.route('/product/:id').delete(deleteProduct);
