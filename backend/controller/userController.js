34494f176e96689ee324a52aba580549863ab685
Rahul phatangare <rahulphatangare01@gmail.com>
2023-11-08 00:36:36 +0530

    created logout api

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -48,9 +48,19 @@ exports.loginUser = catchAsyncError(async (req, res, next) => {
     }
 
     sendToken(user,200,res)
-//   const token = user.getJWTToken();
-//   res.status(200).json({
-//     success: true,
-//     token,
-//   });
+
 });
+
+// logout User
+
+exports.logout = catchAsyncError(async (req,res,next)=>{
+  res.cookie('token',null,{
+    expires:new Date(Date.now()),
+    httpOnly:true,
+  });
+
+  res.status(200).json({
+    success:true,
+    message:"User Logged out Successfully"
+  })
+})
